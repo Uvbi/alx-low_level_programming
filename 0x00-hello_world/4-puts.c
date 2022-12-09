73fd1b1c017158b99bb6dfa68de457af42672447
uvbi <evaorhue@gmail.com>
2022-12-08 18:49:50 -0800

    first line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main - to print a line of code using puts
- *
- * Return: 0
-*/
+
+/**
+  * main - Entry point
+  * Return: Always 0 (Success)
+  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"):
-	return (0):
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
