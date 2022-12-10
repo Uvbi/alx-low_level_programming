bc42bdd2cdc64cecaf5a471ffd6b0d6573bebbc4
uvbi <evaorhue@gmail.com>
2022-12-10 00:46:04 -0800

    mixed cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Print alphabets in lowercase and uppercase.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+
+}
