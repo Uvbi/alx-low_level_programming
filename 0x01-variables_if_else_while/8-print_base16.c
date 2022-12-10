93773dde3d098b319857e49e2161eeb7373463b6
uvbi <evaorhue@gmail.com>
2022-12-10 01:03:48 -0800

    hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints all hexadecimal numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	i = 'a';
+	while (i <= 'f')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+
+}
